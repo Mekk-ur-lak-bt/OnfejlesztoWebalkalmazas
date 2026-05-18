47e888d6418ffa831c88450b7eb811a80222da13
hrvthlevi <horvathleventeroland@gmail.com>
2026-05-18 18:45:54 +0200

    feat: Backend: route-ok bekotese

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+
+const feladatRoutes = require("./routes/feladat");
+const felhasznaloRoutes = require("./routes/felhasznalo");
+const kategoriaRoutes = require("./routes/kategoria");
+
 const app = express();
 const port = 3000;
 
@@ -9,6 +14,10 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ uzenet: "A szerver fut!" });
 });
+
+app.use("/api/feladatok", feladatRoutes);
+app.use("/api/felhasznalok", felhasznaloRoutes);
+app.use("/api/kategoriak", kategoriaRoutes);
 
 app.listen(port, () => {
   console.log(`A szerver fut a ${port} porton!`);
